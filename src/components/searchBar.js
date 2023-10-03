290f022eac613f48bfe0ac8f57043f986f13efa2
mabadia <angel.beyou95@gmail.com>
2023-10-02 18:02:46 -0700

    style update and file update

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '/weather.css'
 
-function SearchBar () {
-const staticPosition = {
-    'position': 'fixed',
-    'top': '0',
-    'text-align': 'center'
-}
-/*let api_key = "ddf9754a5ff837bfe897084619c561bf";
+function SearchBar() {
+    const [location, setLocation] = useState('');
+    const handleSearch = () => {
+        console.log('Searching for:', location)
+    }
 
-const handleSearch = async () =>{
-    const element = document.getElementsByClassName({location})
-}
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${api_key}`;
-let response = await fetch(url);
-let data = response.json();
-*/
+    /*let api_key = "ddf9754a5ff837bfe897084619c561bf";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${api_key}`;
+    let response = await fetch(url);
+    let data = response.json();
+    // const element = document.getElementsByClassName({location})
+    */
+
     return (
-    <div className='searchBar' style={staticPosition}>
-        <input type='text' className='location' placeholder='Search'/>
-        <button type='submit' >SEARCH</button>
-    </div>
-)
+        <div className='searchBar'>
+            <input type='text'
+                className='location'
+                placeholder='Search'
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+            />
+            <button onClick={handleSearch}>SEARCH</button>
+        </div>
+    );
 }
 
 export default SearchBar;
