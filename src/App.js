7bbd5122ebf08e80468663dc3f05d2a727548825
lbayley512 <lbayley512@gmail.com>
2023-10-02 21:07:44 -0700

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import React from 'react';
 import SearchBar from './components/searchBar';
+import NavBar from './components/navBar'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherApp from './components/weather';
+import Hourly from './components/hourly';
+import Weekly from './components/weekly';
+import Monthly from './components/monthly';
 
 function App() {
   return (
     <div className="App">
       <SearchBar/>
-        <WeatherApp/>
+      <Router>
+        <>
+          <NavBar />
+          <Routes>
+            <Route exact path='/' element={<WeatherApp/>}/>
+            <Route exact path='/hourly' element={<Hourly/>}/>
+            <Route exact path='/weekly' element={<Weekly/>}/>
+            <Route exact path='/monthly' element={<Monthly/>}/>
+          </Routes>
+        </>
+      </Router>
+      
     </div>
   );
 }
