26fff9d4a1dd9c5ea31ce7bdad0b7d56584059c6
mabadia <angel.beyou95@gmail.com>
2023-10-04 15:33:49 -0700

    deleted code

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -57,7 +57,4 @@ export default SearchBar;
 
 
 
-// let url = ``;
-// let response = await fetch(url);
-// let data = response.json();
-// const element = document.getElementsByClassName({location})
+
