046cbf4741517b570656cb364b05cc256ba13f55
mabadia <angel.beyou95@gmail.com>
2023-10-06 23:01:20 -0700

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 import SearchBar from './components/searchBar';
 import NavBar from './components/navBar'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CurrentWeather from './components/CurrentWeather';
 import WeatherApp from './pages/weather';
@@ -17,7 +17,7 @@ function App() {
   const [location, setLocation] = useState({ city: '', state: '' });
 
   const updateLocation = (newCity, newState) => {
-      setLocation({ city: newCity, state: newState });
+    setLocation({ city: newCity, state: newState });
   };
   return (
     <div className="App">
@@ -26,18 +26,14 @@ function App() {
         <>
           <NavBar />
           <Routes>
-          <Route
-                            exact
-                            path='/'
-                            element={<CurrentWeather city={location.city} state={location.state} />}
-                        />
-            <Route exact path='/Hourly' element={<Hourly/>}/>
-            <Route exact path='/weekly' element={<Weekly/>}/>
-            <Route exact path='/Locations' element={<Locations/>}/>
+            <Route exact path='/' element={<CurrentWeather city={location.city} state={location.state} />} />
+            <Route exact path='/Hourly' element={<Hourly />} />
+            <Route exact path='/weekly' element={<Weekly />} />
+            <Route exact path='/Locations' element={<Locations />} />
           </Routes>
         </>
       </Router>
-      
+
     </div>
   );
 }
