d33d5b799bd5c623a5988e588328bb5dfff96f4a
lbayley512 <lbayley512@gmail.com>
2023-10-07 12:14:00 -0700

    signUp update

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import { useNavigate } from "react-router-dom";
+
 import '../components/styles/signUp.css'
 
 
@@ -10,12 +11,14 @@ const SignUp = () => {
     const navigate = useNavigate();
     const username = useRef()
     const password = useRef()
+ 
     const handleSubmit = async e => {
         if (username.current.value && password.current.value) {
             localStorage.setItem("username", username.current.value)
             localStorage.setItem("password", password.current.value)
 
         }
+    
         navigate('/')
     };
 
@@ -30,11 +33,11 @@ const SignUp = () => {
                     <h3 className="mb-2">Sign Up</h3>
                     <div className='mb-2'>
                         <label htmlFor='username'>Username:</label>
-                        <input id='input' ref={username} type='username' placeholder='Username' className='form-control' />
+                        <input id='inputSignUp' ref={username} type='username' placeholder='Username' className='form-control' />
                     </div>
                     <div className='mb-2'>
                         <label htmlFor='password'>Password:</label>
-                        <input id='input' ref={password} type='password' placeholder='Password' className='form-control' />
+                        <input id='inputSignUp' ref={password} type='password' placeholder='Password' className='form-control' />
                     </div>
                     <div className='mb-2'>
                         <button type='submit' onClick={handleSubmit}>Sign Up</button>
