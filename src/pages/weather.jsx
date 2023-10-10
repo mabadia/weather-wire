3d385277571873bf91c00d5e18c763442f7ca9cc
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2023-10-09 20:32:51 -0700

    NEW

--- src/pages/weather.jsx
+++ src/pages/weather.jsx
@@ -6,26 +6,8 @@ import CurrentWeather from '../components/CurrentWeather';
 const WeatherApp = ({ currentWeather, error }) => {
   const [userLocation, setUserLocation] = useState(null);
 
-  useEffect(() => {
-    // Check if the browser supports geolocation
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          // Get the user's latitude and longitude
-          const { latitude, longitude } = position.coords;
-          setUserLocation({ latitude, longitude });
-        },
-        (error) => {
-          console.error('Error getting user location:', error);
-          // Handle the error here, e.g., show an error message to the user
-        }
-      );
-    } else {
-      console.error('Geolocation is not supported by your browser.');
-      // Handle the case where geolocation is not supported
-    }
-  }, []);
 
+  
 
   return (
     <div className="app">
